b4ec3feac5cc88dbafa339709c26d4cc2c49e318
ArmandoRSD <armadiucs114200@gmail.com>
2025-09-05 13:38:24 -0600

    Add files via upload

---
+++ Estructuraact3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sistemas.listas.estructuraact3;
+
+/**
+ *
+ * @author arman
+ */
+public class Estructuraact3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
